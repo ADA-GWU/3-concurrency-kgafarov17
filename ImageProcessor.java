49a0b3cc58a1d5d82b62184b5c28bdad8cd5a837
kgafarov17 <98353418+kgafarov17@users.noreply.github.com>
2023-11-16 20:16:42 +0400

    update ImageProcessor.java

--- ImageProcessor.java
+++ ImageProcessor.java
@@ -3,23 +3,25 @@ import java.awt.image.BufferedImage;
 import java.awt.image.Raster;
 import java.awt.image.WritableRaster;
 import java.io.IOException;
-import java.io.File;
+import java.util.function.Consumer;
 import javax.imageio.ImageIO;
+import java.io.File;
 
 class ImageProcessor implements Runnable {
 
+    private static final int SLEEP_DURATION = 20;
     private BufferedImage img; // original image that is going to be pixelated
     private int pixelSize; // the size of the pixel blocks
-    private JLabel imageLabel; // provides a visual indication of the progress of the pixelation process.
+    private Consumer<BufferedImage> imageUpdateCallback; // callback to update the image
     private int startY; // starting row
     private int endY; //ending row
     private Raster src; // allows access to pixel data in the original image for processing.
     private WritableRaster dest; // used to store the pixel data of the modified (pixelated) image.
 
-    public ImageProcessor(BufferedImage img, int pixelSize, JLabel imageLabel, int startY, int endY, Raster src, WritableRaster dest) {
+    public ImageProcessor(BufferedImage img, int pixelSize, Consumer<BufferedImage> imageUpdateCallback, int startY, int endY, Raster src, WritableRaster dest) {
         this.img = img;
         this.pixelSize = pixelSize;
-        this.imageLabel = imageLabel;
+        this.imageUpdateCallback = imageUpdateCallback;
         this.startY = startY;
         this.endY = endY;
         this.src = src;
@@ -37,16 +39,17 @@ class ImageProcessor implements Runnable {
                         dest.setPixel(xPixel, yPixel, sum);
                     }
                 }
+
                 try {
-                    Thread.sleep(20); // delay for visual efect
+                    Thread.sleep(SLEEP_DURATION); // delay for visual effect
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
+
                 img.setData(dest); // update the image data with the modified pixel values
-                imageLabel.repaint();
+                SwingUtilities.invokeLater(() -> imageUpdateCallback.accept(img)); // invokeLater to update the UI
             }
         }
-
 
         try {
             saveImage("result.jpg"); //save pixelated image to result.jpg
@@ -55,25 +58,25 @@ class ImageProcessor implements Runnable {
         }
     }
 
-
     private double[] getAverageColor(int y, int x) {
         double[] temp = new double[3]; // temp value for each pixel RGB
         double[] averageColour = new double[3]; // average colour of the area
         int cnt = 0;
+
         for(int yPixel = y; (yPixel < y + pixelSize) && (yPixel < dest.getHeight()); yPixel++) {
             for(int xPixel = x; (xPixel < x + pixelSize) && (xPixel < dest.getWidth()); xPixel++) {
-                src.getPixel(xPixel,yPixel, temp);
-                averageColour[0]+= temp[0];
-                averageColour[1]+= temp[1];
-                averageColour[2]+= temp[2];
+                src.getPixel(xPixel, yPixel, temp);
+                averageColour[0] += temp[0];
+                averageColour[1] += temp[1];
+                averageColour[2] += temp[2];
                 cnt++;
             }
-
         }
 
-        averageColour[0]= averageColour[0]/cnt;
-        averageColour[1]= averageColour[1]/cnt;
-        averageColour[2]= averageColour[2]/cnt;
+        for (int i = 0; i < 3; i++) {
+            averageColour[i] /= cnt;
+        }
+
         return averageColour;
     }
 
